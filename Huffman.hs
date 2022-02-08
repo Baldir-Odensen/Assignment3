4e5ab80253cf22294e598a1ab9f69d40361dd9c2
jogr2046 <johan.grandin.2046@student.uu.se>
2022-02-08 14:20:49 +0100

    .

--- Huffman.hs
+++ Huffman.hs
@@ -21,6 +21,7 @@ type BitCode = [Bool]
    PRE: acc >= 0
    RETURNS: acc of how many times specified char occurs in str.
    EXAMPLES: characterCountsAux "" 'h' 0         == 0
+             characterCountsAux "xxx" 'x' 0      == 3
              characterCountsAux "hej hopp" 'h' 0 == 2
              characterCountsAux "hej hopp" 'o' 0 == 1
 -}
@@ -33,6 +34,7 @@ characterCountsAux (x:xs) y acc | y == x = 1 + characterCountsAux xs y acc
    RETURNS: a table that maps each character that occurs in s to the number of
          times the character occurs in s
    EXAMPLES: characterCounts "" == T []
+             characterCounts "xxx" == T [('x',3)]
              characterCounts "hej hopp" == T [('p',2),('o',1),('h',2),(' ',1),('j',1),('e',1)]
              characterCounts "this is an example of a huffman tree" == T [('e',4),('r',1),('t',2),(' ',7),('n',2),('a',4),('m',2),('f',3),('u',1),('h',2),('o',1),('l',1),('p',1),('x',1),('s',2),('i',2)]
  -}
@@ -49,12 +51,15 @@ characterCounts (x:xs) = Table.insert (characterCounts xs) x (characterCountsAux
 -}
 data HuffmanTree = Void
                  | Leaf Char Int
-                 | Node Int HuffmanTree HuffmanTree deriving Show
+                 | Node Int HuffmanTree HuffmanTree deriving (Show, Eq)
 
 {- priorityQ lst
    Creates a PriorityQueue out of a specified list of tuples.
    RETURNS: PriorityQueue from lst
-   EXAMPLES:
+   EXAMPLES: priorityQ (Table.iterate (characterCounts "") (\y x -> x : y) []) == BinoHeap []
+             priorityQ (Table.iterate (characterCounts "xxx") (\y x -> x : y) []) == BinoHeap [Node 0 3 (Leaf 'x' 3) []]
+             priorityQ (Table.iterate (characterCounts "hej hopp") (\y x -> x : y) []) == BinoHeap [Node 1 1 (Leaf 'j' 1) [Node 0 1 (Leaf 'e' 1) []],Node 2 1 (Leaf 'o' 1) [Node 1 1 (Leaf ' ' 1) [Node 0 2 (Leaf 'h' 2) []],Node 0 2 (Leaf 'p' 2) []]]
+             priorityQ (Table.iterate (characterCounts "this is an example of a huffman tree") (\y x -> x : y) []) == BinoHeap [Node 4 1 (Leaf 'r' 1) [Node 3 1 (Leaf 'u' 1) [Node 2 1 (Leaf 'p' 1) [Node 1 2 (Leaf 's' 2) [Node 0 2 (Leaf 'i' 2) []],Node 0 1 (Leaf 'x' 1) []],Node 1 1 (Leaf 'o' 1) [Node 0 1 (Leaf 'l' 1) []],Node 0 2 (Leaf 'h' 2) []],Node 2 2 (Leaf 'n' 2) [Node 1 2 (Leaf 'm' 2) [Node 0 3 (Leaf 'f' 3) []],Node 0 4 (Leaf 'a' 4) []],Node 1 2 (Leaf 't' 2) [Node 0 7 (Leaf ' ' 7) []],Node 0 4 (Leaf 'e' 4) []]]
 -}
 priorityQ :: [(Char,Int)] -> PriorityQueue HuffmanTree
 -- VARIANT: length lst
@@ -62,13 +67,18 @@ priorityQ [] = PriorityQueue.empty
 priorityQ (x:xs) = PriorityQueue.insert (priorityQ xs) (Leaf (fst x) (snd x),snd x)
 
 {- mergeQ pq acc
-   Merges all of the trees in a specified PriorityQueue into one tree based on their priority.
-   RETURNS: pg with only one tree containing all previous trees
-   EXAMPLES:
+   Merges the specified number of trees in a specified PriorityQueue into one tree based on their priority.
+   PRE: acc >= 0
+   RETURNS: pg with acc number of trees merged into one tree
+   EXAMPLES: mergeQ (priorityQ (Table.iterate (characterCounts "") (\y x -> x : y) [])) (sizeQ (priorityQ (Table.iterate (characterCounts "") (\y x -> x : y) []))) == BinoHeap []
+             mergeQ (priorityQ (Table.iterate (characterCounts "xxx") (\y x -> x : y) [])) (sizeQ (priorityQ (Table.iterate (characterCounts "xxx") (\y x -> x : y) []))) == BinoHeap [Node 0 3 (Leaf 'x' 3) []]
+             mergeQ (priorityQ (Table.iterate (characterCounts "hej hopp") (\y x -> x : y) [])) (sizeQ(priorityQ (Table.iterate (characterCounts "hej hopp") (\y x -> x : y) []))) == BinoHeap [Node 0 8 (Node 8 (Node 4 (Node 2 (Leaf 'o' 1) (Leaf 'j' 1)) (Leaf 'p' 2)) (Node 4 (Leaf 'h' 2) (Node 2 (Leaf ' ' 1) (Leaf 'e' 1)))) []]
+             mergeQ (priorityQ (Table.iterate (characterCounts "this is an example of a huffman tree") (\y x -> x : y) [])) (sizeQ (priorityQ (Table.iterate (characterCounts "this is an example of a huffman tree") (\y x -> x : y) []))) == BinoHeap [Node 0 36 (Node 36 (Node 16 (Node 8 (Node 4 (Leaf 'i' 2) (Leaf 'm' 2)) (Node 4 (Leaf 'n' 2) (Node 2 (Leaf 'x' 1) (Leaf 'l' 1)))) (Node 8 (Node 4 (Leaf 's' 2) (Node 2 (Leaf 'r' 1) (Leaf 'u' 1))) (Leaf 'e' 4))) (Node 20 (Node 8 (Node 4 (Leaf 'h' 2) (Leaf 't' 2)) (Leaf 'a' 4)) (Node 12 (Node 5 (Node 2 (Leaf 'p' 1) (Leaf 'o' 1)) (Leaf 'f' 3)) (Leaf ' ' 7)))) []]
 -}
 mergeQ :: PriorityQueue HuffmanTree -> Int -> PriorityQueue HuffmanTree
 -- VARIANT: acc-1
 mergeQ pq 0 = pq
+mergeQ pq 1 = pq
 mergeQ pq acc = let a = PriorityQueue.least
                     b = fst (PriorityQueue.least pq)
                     c = snd (PriorityQueue.least pq)
@@ -78,15 +88,20 @@ mergeQ pq acc = let a = PriorityQueue.least
 {- sizeQ pg
    Calculates how many trees there are in a specified queue.
    RETURNS: Int of how many trees in pq
-   EXAMPLES:
+   EXAMPLES: sizeQ (priorityQ (Table.iterate (characterCounts "") (\y x -> x : y) [])) == 0
+             sizeQ (priorityQ (Table.iterate (characterCounts "xxx") (\y x -> x : y) [])) == 1
+             sizeQ(priorityQ (Table.iterate (characterCounts "hej hopp") (\y x -> x : y) [])) == 6
+             sizeQ (priorityQ (Table.iterate (characterCounts "this is an example of a huffman tree") (\y x -> x : y) [])) == 16
 -}
 sizeQ :: PriorityQueue HuffmanTree -> Int
 -- VARIANT: length of queue
-sizeQ pq = if is_empty (snd (PriorityQueue.least pq))
+sizeQ pq = if is_empty (pq)
             then 0
             else 1 + sizeQ (snd (PriorityQueue.least pq))
 
-x = characterCounts "this is an example of a huffman tree"
+x = characterCounts ""
+y = characterCounts "xxx"
+z = characterCounts "this is an example of a huffman tree"
 
 {- huffmanTree table
    Creates a HuffmanTree from a table.
@@ -95,7 +110,9 @@ x = characterCounts "this is an example of a huffman tree"
    EXAMPLES:
  -}
 huffmanTree :: Table Char Int -> HuffmanTree
-huffmanTree t = fst(fst(PriorityQueue.least (mergeQ (priorityQ (reverse(Table.iterate t (\y x -> x : y) [])))(sizeQ (priorityQ (reverse(Table.iterate t (\y x -> x : y) [])))))))
+huffmanTree t = if is_empty (priorityQ (Table.iterate t (\y x -> x : y) []))
+                  then Void
+                  else fst(fst(PriorityQueue.least (mergeQ (priorityQ (Table.iterate t (\y x -> x : y) []))(sizeQ (priorityQ (Table.iterate t (\y x -> x : y) []))))))
 
 {- codeTableAux
    
